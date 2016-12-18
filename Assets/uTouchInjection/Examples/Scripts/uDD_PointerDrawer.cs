9804e76e1a2df5f118affb1bb731e86c7858f114
hecomi <info@hecomi.com>
2016-12-18 14:55:22 +0900

    add cursor.

--- Assets/uTouchInjection/Examples/Scripts/uDD_PointerDrawer.cs
+++ Assets/uTouchInjection/Examples/Scripts/uDD_PointerDrawer.cs
@@ -8,11 +8,15 @@ public class uDD_PointerDrawer : MonoBehaviour
     uDD_TouchDispatcher dispatcher_;
     LineRenderer line_;
 
+    [SerializeField] GameObject cursor;
     [SerializeField] float nonHitAlpha = 0.2f;
     [SerializeField] float hitAlpha = 0.5f;
     [SerializeField] Color releaseColor = new Color(0.5f, 0.5f, 0.5f);
     [SerializeField] Color hoverColor = new Color(0.1f, 0.5f, 0.2f);
     [SerializeField] Color touchColor = new Color(0.8f, 0.2f, 0.1f);
+
+    Color color_;
+    Material cursorMaterial_;
 
     void Start()
     {
@@ -22,8 +26,21 @@ public class uDD_PointerDrawer : MonoBehaviour
 
     void Update()
     {
-        UpdateLine();
         UpdateColor();
+        UpdateLine();
+        UpdateCursor();
+    }
+
+    void UpdateColor()
+    {
+        switch (dispatcher_.state) {
+            case State.Release : color_ = releaseColor; break;
+            case State.Hover   : color_ = hoverColor;   break;
+            case State.Touch   : color_ = touchColor;   break;
+        }
+
+        color_.a = dispatcher_.result.hit ? hitAlpha : nonHitAlpha;
+        color_.a *= dispatcher_.isPrimaryPointer ? 1f : 0.5f;
     }
 
     void UpdateLine()
@@ -35,21 +52,35 @@ public class uDD_PointerDrawer : MonoBehaviour
         } else {
             line_.SetPosition(1, transform.position + transform.forward * 0.5f);
         }
+
+        line_.SetColors(color_, color_);
     }
 
-    void UpdateColor()
+    void UpdateCursor()
     {
-        var color = Color.gray;
+        if (cursor == null) return;
 
-        switch (dispatcher_.state) {
-            case State.Release : color = releaseColor; break;
-            case State.Hover   : color = hoverColor;   break;
-            case State.Touch   : color = touchColor;   break;
+        if (cursorMaterial_ == null) {
+            cursorMaterial_ = cursor.GetComponent<Renderer>().material;
         }
 
-        color.a = dispatcher_.result.hit ? hitAlpha : nonHitAlpha;
-        color.a *= dispatcher_.isPrimaryPointer ? 1f : 0.5f;
+        if (dispatcher_.state == State.Release) {
+            cursor.SetActive(false);
+            return;
+        }
 
-        line_.SetColors(color, color);
+        var result = dispatcher_.result;
+        var texture = result.texture;
+        var coord = dispatcher_.filteredDesktopCoord;
+        coord.x -= texture.monitor.left;
+        coord.y -= texture.monitor.top;
+        var pos = texture.GetWorldPositionFromCoord(coord);
+        cursor.transform.position = pos + result.normal * 0.01f;
+        cursor.transform.rotation = Quaternion.LookRotation(result.normal, result.texture.transform.up);
+        cursor.SetActive(true);
+
+        var color = color_;
+        color.a *= 0.2f;
+        cursorMaterial_.SetColor("_TintColor", color);
     }
 }
