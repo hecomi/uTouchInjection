3f640a859e3c2864fe8637901e659ef1d329f82f
hecomi <info@hecomi.com>
2019-10-03 23:23:49 +0900

    add APIs to change debug log mode.

--- Scripts/Lib.cs
+++ Scripts/Lib.cs
@@ -5,31 +5,49 @@
 namespace uTouchInjection
 {
 
+public enum DebugMode
+{
+    None = 0,
+    File = 1,
+    UnityLog = 2,
+}
+
 public static class Lib
 {
-    [DllImport("uTouchInjection")]
+    const string dllName = "uTouchInjection";
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate void DebugLogDelegate(string str);
+
+    [DllImport(dllName)]
+    public static extern void SetDebugMode(DebugMode mode);
+    [DllImport(dllName)]
+    public static extern void SetLogFunc(DebugLogDelegate func);
+    [DllImport(dllName)]
+    public static extern void SetErrorFunc(DebugLogDelegate func);
+    [DllImport(dllName)]
     public static extern void Initialize(int touchNum);
-    [DllImport("uTouchInjection")]
+    [DllImport(dllName)]
     public static extern void Finalize();
-    [DllImport("uTouchInjection")]
+    [DllImport(dllName)]
     public static extern void Update();
-    [DllImport("uTouchInjection")]
+    [DllImport(dllName)]
     public static extern void SetPosition(int id, int x, int y);
-    [DllImport("uTouchInjection")]
+    [DllImport(dllName)]
     public static extern void SetPressure(int id, int pressure);
-    [DllImport("uTouchInjection")]
+    [DllImport(dllName)]
     public static extern void SetAreaSize(int id, int areaSize);
-    [DllImport("uTouchInjection")]
+    [DllImport(dllName)]
     public static extern void SetOrientation(int id, int degree);
-    [DllImport("uTouchInjection")]
+    [DllImport(dllName)]
     public static extern void Touch(int id);
-    [DllImport("uTouchInjection")]
+    [DllImport(dllName)]
     public static extern void Hover(int id);
-    [DllImport("uTouchInjection")]
+    [DllImport(dllName)]
     public static extern void Release(int id);
-    [DllImport("uTouchInjection")]
+    [DllImport(dllName)]
     public static extern void EnableLogOutput();
-    [DllImport("uTouchInjection")]
+    [DllImport(dllName)]
     public static extern void DisableLogOutput();
 }
 
